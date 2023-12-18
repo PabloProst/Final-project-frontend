eba6bea5d3a6cacc204cd69e34b500137d687251
PabloProst <pabloprost16@gmail.com>
2023-12-18 15:36:01 +0100

    feat: offcanvas login and register added on navbar

--- src/common/PageNavbar/PageNavbar.jsx
+++ src/common/PageNavbar/PageNavbar.jsx
@@ -1,29 +1,90 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 export const PageNavbar = () => {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Trail of Bombs</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Ranking</Nav.Link>
-            <NavDropdown title="Login" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Register</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Contact</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
 
+  const handleLoginClick = () => {
+    setShowLogin(!showLogin);
+    setShowRegister(false);
+  };
+
+  const handleRegisterClick = () => {
+    setShowRegister(!showRegister);
+    setShowLogin(false);
+  };
+
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Trail of Bombs</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto"> {/* Cambiado de 'me-auto' a 'ms-auto' para alinear a la derecha */}
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#link">Ranking</Nav.Link>
+              <NavDropdown title="Login" id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={handleLoginClick}>Login</NavDropdown.Item>
+                <NavDropdown.Item onClick={handleRegisterClick}>Register</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Contact</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Offcanvas show={showLogin} onHide={() => setShowLogin(false)} placement="end">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Login</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <form>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" name="email" />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </form>
+        </Offcanvas.Body>
+      </Offcanvas>
+
+      <Offcanvas show={showRegister} onHide={() => setShowRegister(false)} placement="end">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Register</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <form>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" />
+
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" name="email" />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+
+            <Button variant="primary" type="submit">
+              Register
+            </Button>
+          </form>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+};

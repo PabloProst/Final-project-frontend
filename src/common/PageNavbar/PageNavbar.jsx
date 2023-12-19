1336af16b1911382bedd4858f335d6b4c0fc3b20
PabloProst <pabloprost16@gmail.com>
2023-12-19 17:42:34 +0100

    feat: title on main screen and btn play

--- src/common/PageNavbar/PageNavbar.jsx
+++ src/common/PageNavbar/PageNavbar.jsx
@@ -5,6 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import './PageNavbar.css';
 
 export const PageNavbar = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -27,11 +28,11 @@ export const PageNavbar = () => {
           <Navbar.Brand href="#home">Trail of Bombs</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto"> {/* Cambiado de 'me-auto' a 'ms-auto' para alinear a la derecha */}
+            <Nav className="ms-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">About</Nav.Link>
               <Nav.Link href="#link">Ranking</Nav.Link>
-              <NavDropdown title="Login" id="basic-nav-dropdown">
+              <NavDropdown title="Account" id="basic-nav-dropdown">
                 <NavDropdown.Item onClick={handleLoginClick}>Login</NavDropdown.Item>
                 <NavDropdown.Item onClick={handleRegisterClick}>Register</NavDropdown.Item>
                 <NavDropdown.Divider />
@@ -41,48 +42,40 @@ export const PageNavbar = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
       <Offcanvas show={showLogin} onHide={() => setShowLogin(false)} placement="end">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Login</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <form>
-            <label htmlFor="email">Email:</label>
-            <input type="text" id="email" name="email" />
-
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
-
-            <Button variant="primary" type="submit">
-              Login
-            </Button>
-          </form>
+          <div className='offcanvas-design'>
+              <label htmlFor="email">Email:</label>
+              <input type="text" id="email" name="email" />
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" />
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
-
       <Offcanvas show={showRegister} onHide={() => setShowRegister(false)} placement="end">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Register</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <form>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
-
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
-
-            <label htmlFor="email">Email:</label>
-            <input type="text" id="email" name="email" />
-
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
-
-            <Button variant="primary" type="submit">
-              Register
-            </Button>
-          </form>
+          <div className='offcanvas-design'>
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" />
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" name="username" />
+              <label htmlFor="email">Email:</label>
+              <input type="text" id="email" name="email" />
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" />
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
-import {React} from 'react';
+import React from 'react';
 import "./Home.css";
 import { PageNavbar } from '../../common/PageNavbar/PageNavbar';
 
 export const Home = () => {
   return (
     <div className='home-design'>
-        <PageNavbar/>
-      <div>Holaaa</div>
+      <PageNavbar/>
+      <div className='home-container'>
+        <img src='./src/assets/img/logo.png' alt='Logo' className='image-logo'></img>
+        <button className='btn-play'>PLAY</button>
+      </div>
     </div>
   );
 }
